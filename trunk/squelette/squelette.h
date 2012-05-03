1c2e8056792c5d10d602de2e077fde42fd326280
etudiant <beilleauquentin@gmail.com>
2012-05-03 10:33:01 +0200

    mise à jour du 03/05/2012 à 10h26

--- trunk/squelette/squelette.h
+++ trunk/squelette/squelette.h
@@ -17,7 +17,15 @@ public:
         void resizeEvent(QResizeEvent *event);
        // void grabMouse();// attention pas virutel !! agit qd le bouton de la souris est maintenu il agit sur cursor (type QCursor) setcursor permet de le modifié
         void mouseReleaseEvent(QMouseEvent * event );// agit qd le bouton de la souris est relaché
-        void  mouseMoveEvent ( QMouseEvent * event );
+        void mouseMoveEvent ( QMouseEvent * event );//est appelé chaque fois que la souris se déplace avec l'un de ses boutons maintenu enfoncé
+        void wheelEvent ( QWheelEvent * event );//utiliser pour la molette de la souris
+        void moveEvent(QMoveEvent * event); // est appelé quand le widget a été déplacé, relativement à son parent ;
+        //QPoint QWidget::mapFromGlobal ( const QPoint & pos ) const// permet de recupéré les coordonné de la souris dans le widget
+
+        /*//utile si on a plusieurs sous fenetre ce qui permet de faire un traitement uniquement dans la fenetre voulu
+        void enterEvent(QEvent * event);// est appelé quand la souris entre dans l'espace de l'écran du widget, cela inclut l'espace de l'écran détenu par tout enfant de ce widget ;
+        void leaveEvent();// est appelé quand la souris quitte l'espace de l'écran du widget ; si la souris entre dans un widget enfant, cela ne causera pas un leaveEvent() ;
+        */
 
     signals :
 
