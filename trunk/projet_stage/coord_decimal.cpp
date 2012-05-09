07cd47d164340266872205f4a74a6890b65801ff
etudiant <beilleauquentin@gmail.com>
2012-05-09 10:12:18 +0200

    mise à jour du 09/05/2012 à 10h12

--- trunk/projet_stage/coord_decimal.cpp
+++ trunk/projet_stage/coord_decimal.cpp
@@ -1,5 +1,30 @@
 #include "coord_decimal.h"
 
-coord_decimal::coord_decimal()
+coord_decimal::coord_decimal():longitude(0),latitude(0)
 {
+}
+
+
+
+//geteurs
+float coord_decimal::getLatitude()
+{
+    return latitude;
+}
+
+float coord_decimal::getLongitude()
+{
+    return longitude;
+}
+
+
+//seteurs
+void coord_decimal::setLatitude(float la)
+{
+    latitude = la;
+}
+
+void coord_decimal::setLongitude(float lo)
+{
+    longitude = lo;
 }
--- trunk/projet_stage/coord_decimal.h
+++ trunk/projet_stage/coord_decimal.h
@@ -3,8 +3,27 @@
 
 class coord_decimal
 {
+private:
+    float latitude;
+    float longitude;
+
+
 public:
     coord_decimal();
+    // getteurs
+    float getLatitude();
+    float getLongitude();
+
+
+    //seteurs
+    void setLatitude(float la);
+    void setLongitude(float lo);
+
+
+
+
+
+
 };
 
 #endif // COORD_DECIMAL_H
